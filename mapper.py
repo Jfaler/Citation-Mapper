8bb4bb6cdb09f7fdef5ce578a58749a4cdc9f6e4
Justin Faler <jfaler216@student.msjc.edu>
2020-04-08 01:24:00 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,33 +1,36 @@
 '''
 Programmer: Justin Faler
-Date: 4/7/2020
+Date: 4/8/2020
 Description: PDF Citation Map
 '''
 
 import numpy as np
 import networkx as nx
+import matplotlib as mpl
 import pdfminer, re, sys, os, time, csv
 import matplotlib.pyplot as plt
 from refextract import extract_references_from_file
 
+plt.rcParams["figure.facecolor"] = "#333333"
 
-citation_map = nx.Graph(directed=True)
-references = extract_references_from_file('/home/gadfly/Documents/Books/math.pdf')
+citation_map = nx.DiGraph(directed=True)
+references = extract_references_from_file('/home/user/Documents/Books/example.pdf')
 
 options = {
-    'node_color': '#28a7ea',
-    'node_size': 600,
-    'width': 1,
+    'node_color': '#36DBCA',
+    'node_size': 150,
+    'width': 0.2,
+    'alpha': 1,
     'arrowstyle': '-|>',
-    'arrowsize': 25,
+    'arrowsize': 15,
 }
 
-base = os.path.basename('/home/gadfly/Documents/Books/math.pdf')
+base = os.path.basename('/home/user/Documents/Books/example.pdf')
 name = os.path.splitext(base)[0]
 
 i = 1
 
-while (i < 10):
+while (i < 30):
     i = i + 1
     citations = references[i]['raw_ref']
     print(citations)
@@ -35,5 +38,8 @@ while (i < 10):
     citation_map.add_nodes_from(citations)
 
 # Layout
-nx.draw_networkx(citation_map, arrows=True, **options)
+pos = nx.spring_layout(citation_map)
+nx.draw_networkx(citation_map, ChildProcessError, arrows=True, **options)
+plt.tight_layout()
+plt.axis('off')
 plt.show()
